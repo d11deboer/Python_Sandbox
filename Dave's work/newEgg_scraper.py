9b317174384834fe53c408a0b3c5239e264abbda
David D <46694467+d11deboer@users.noreply.github.com>
2019-07-15 18:23:14 -0700

    update

--- Dave's work/newEgg_scraper.py
+++ Dave's work/newEgg_scraper.py
@@ -4,3 +4,14 @@ from bs4 import BeautifulSoup
 
 #fetching the book scraping website page with the book info
 req = requests.get('https://www.newegg.com/p/pl?d=ram')
+
+#creating the soup from which to fetch the book data
+soup = BeautifulSoup(req.content, 'html.parser')
+
+#closing the connection
+req.close()
+
+#finding the ordered list with the books
+container = soup.find('div', class_ = 'items-view is-grid')
+
+print(container)
