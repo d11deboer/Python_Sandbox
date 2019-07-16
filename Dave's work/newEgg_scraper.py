05d98c51f709e2b2ea89d4d62a61f7f61f9d9b49
David D <46694467+d11deboer@users.noreply.github.com>
2019-07-15 19:38:02 -0700

    tested complete scrape of newEgg

--- Dave's work/newEgg_scraper.py
+++ Dave's work/newEgg_scraper.py
@@ -14,4 +14,32 @@ req.close()
 #finding the ordered list with the books
 container = soup.find('div', class_ = 'items-view is-grid')
 
-print(container)
+#fetching the list of all the items
+list = container.findAll('div', class_ = 'item-container')
+
+#creating the array of all the full titles of the items
+name_array = [item.find('img')['title'] for item in list]
+#print(name_array) #test print
+
+#creating the array of all the prices of the items
+price_array = ['$' + item.find('li', class_ = 'price-current').strong.text for item in list]
+#print(price_array) #test print
+
+#creating the array of all the ratings of the items
+rating_array = [item.find('i', class_ = 'rating')['class'][1][7] for item in list]
+#print(rating_array) #test print
+
+#creating the array of all the brands of the items
+title_array = [item.find('a', class_ = 'item-brand').img['title'] for item in list]
+#print(title_array) #test print
+
+ram_information = pd.DataFrame({
+	 'Full Title' : name_array,
+	 'Price' : price_array,
+	 'Brand' : title_array,
+	 'Rating' : rating_array 
+	})
+	
+print(ram_information)
+
+ram_information.to_csv('ram_information.csv')
